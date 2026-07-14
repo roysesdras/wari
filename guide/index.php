baf3c2a6e85c41f9fd56193b2cf0473c3d81cb42
Roysesdras <roysesdras49@gmail.com>
2026-07-14 17:34:51 +0000

    style: correction de la variable de couleur orange de l'accentuation wari dans le guide

--- guide/index.php
+++ guide/index.php
@@ -267,7 +267,7 @@ if ($user_id) {
         
         <div class="hero">
             <h1>Le Manuel de la Discipline</h1>
-            <p><strong>IMPORTANT A SAVOIR :</strong> Wari n'est pas un compte bancaire ni un portefeuille électronique (comme Mobile Money), mais un simulateur et un outil de suivi visuel. Votre argent réel reste en totale sécurité dans vos propres poches ou sur vos comptes bancaires habituels.</p>
+            <p><strong style="color: var(--accent);">IMPORTANT A SAVOIR :</strong> Wari n'est pas un compte bancaire ni un portefeuille électronique (comme Mobile Money), mais un simulateur et un outil de suivi visuel. Votre argent réel reste en totale sécurité dans vos propres poches ou sur vos comptes bancaires habituels.</p>
         </div>
 
         <!-- 1. La Répartition -->
