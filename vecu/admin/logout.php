9223c4af671e0d7686135cf4336a578d0db8e898
Roysesdras <roysesdras49@gmail.com>
2026-07-15 17:53:17 +0000

    fix: suppression du cookie vecu_admin_remember lors de la deconnexion admin

--- vecu/admin/logout.php
+++ vecu/admin/logout.php
@@ -1,5 +1,10 @@
 <?php
 session_start();
+
+if (isset($_COOKIE['vecu_admin_remember'])) {
+    setcookie('vecu_admin_remember', '', time() - 3600, "/");
+}
+
 session_destroy();
 header('Location: login.php');
 exit;
